df37a713b7e3876739d8d15f9664d39e20a650a9
Saras <andhinisaras@gmail.com>
2013-11-30 14:09:26 +0700

    Added category page

--- wp-content/themes/upBootstrap3WP-master/page-templates/category-page.php
+++ wp-content/themes/upBootstrap3WP-master/page-templates/category-page.php
@@ -29,6 +29,106 @@ get_header(); ?>
 					?>
 				</div><!-- .entry-content -->
 				<?php edit_post_link( __( 'Edit', 'upbootwp' ), '<footer class="entry-meta"><span class="edit-link">', '</span></footer>' ); ?>
+
+				<div class="row">
+
+					<div class="col-md-4">
+						<div class="category">
+							<p><center><img src="<?php echo get_template_directory_uri();?>/img/icon-gadgets.png" /></center></p>
+							<center>
+								<h3>Gadget</h3>
+								<p>
+									Lorem ipsum dolor sit amet consectetuer a dipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet
+								</p>
+								<button type="button" class="btn btn-primary btn-category">BACA</button>
+							</center>
+						</div>						
+					</div>
+
+
+
+					<div class="col-md-4">
+						<div class="category">
+							<p><center><img src="<?php echo get_template_directory_uri();?>/img/icon-socmed.png" /></center></p>
+							<center>
+								<h3>Social Media</h3>
+								<p>
+									Lorem ipsum dolor sit amet consectetuer a dipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet
+								</p>
+								<button type="button" class="btn btn-primary btn-category">BACA</button>
+							</center>
+						</div>
+					</div>
+
+
+
+					<div class="col-md-4">
+						<div class="category">
+							<p><center><img src="<?php echo get_template_directory_uri();?>/img/icon-UU_ITE.png" /></center></p>
+							<center>
+							<h3>UU ITE</h3>
+								<p>
+									Lorem ipsum dolor sit amet consectetuer a dipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet
+								</p>
+								<button type="button" class="btn btn-primary btn-category">BACA</button>
+							</center>
+						</div>
+					</div>
+
+				</div>
+
+				<br>
+
+				<div class="row">
+					<div class="line"></div>
+				</div>
+
+				<div class="row">
+
+					<div class="col-md-4">
+						<div class="category">
+							<p><center><img src="<?php echo get_template_directory_uri();?>/img/icon-security.png" /></center></p>
+							<center>
+							<h3>Security</h3>
+								<p>
+									Lorem ipsum dolor sit amet consectetuer a dipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet
+								</p>
+								<button type="button" class="btn btn-primary btn-category">BACA</button>
+							</center>
+
+						</div>
+					</div>
+
+					<div class="col-md-4">
+						<div class="category">
+							<p><center><img src="<?php echo get_template_directory_uri();?>/img/icon-app.png" /></center></p>
+							<center>
+							<h3>Apps & OS</h3>
+								<p>
+									Lorem ipsum dolor sit amet consectetuer a dipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet
+								</p>
+								<button type="button" class="btn btn-primary btn-category">BACA</button>
+							</center>
+						</div>
+					</div>
+
+					<div class="col-md-4">
+						<div class="category">
+							<p><center><img src="<?php echo get_template_directory_uri();?>/img/icon-opini.png" /></center></p>
+							<center>
+							<h3>Opini</h3>
+								<p>
+									Lorem ipsum dolor sit amet consectetuer a dipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet
+								</p>
+								<button type="button" class="btn btn-primary btn-category">BACA</button>
+							</center>
+						</div>
+					</div>	
+
+				</div>				
+				
+
+	
 	
 			</div><!-- .col-md-12 -->
 		</div><!-- .row -->
